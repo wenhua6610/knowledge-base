5190addfcf75eb3d0efa05a0adb01c7ee47fec1f
Developer <dev@knowledge-base.local>
2026-05-05 14:10:31 +0800

    fix: include content files in Vercel serverless bundle

--- next.config.js
+++ next.config.js
@@ -1,6 +1,11 @@
 /** @type {import('next').NextConfig} */
 const config = {
   pageExtensions: ['ts', 'tsx', 'mdx'],
+  experimental: {
+    outputFileTracingIncludes: {
+      '/': ['./content/**/*'],
+    },
+  },
 }
 
 module.exports = config
